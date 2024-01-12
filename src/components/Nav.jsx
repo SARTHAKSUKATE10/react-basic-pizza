2d813f854927d862737e1f031511355524a12f43
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2024-01-13 00:11:38 +0530

    assets imported

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='navbar'>
+      <div className='leftside'>
+        <img src={} />
+      </div>
+      <div className='rightside'></div>
+    </div>
+  )
+}
+
+export default Nav
