9dede16c16734bdabaa4bc1af0179c2cf84cb5c6
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2024-01-13 00:14:32 +0530

    assets added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <div className='navbar'>
       <div className='leftside'>
-        <img src={} />
+        {/* <img src={} /> */}
       </div>
       <div className='rightside'></div>
     </div>
