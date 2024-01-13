cc3c2ce7d3786be6f1845b97b351da18e07d3558
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2024-01-13 15:51:27 +0530

    home page and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Route path="/" exact component={Home} />
           </Switch>
         </Router>
+        <Footer/>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import BannerImage from '../assets/pizza.jpeg'
+import '../styles/home.css'
 function Home() {
   return (
-    <div>
-      hello world
+    <div className='home' style={{backgroundImage:`url(${BannerImage})` }}>
+      <div className='headerContainer' >
+        <h1>Shawn's Pizzeria</h1>
+        <p>PIZZA TO FiT ANY TASTE</p>
+        <Link to='/menu'><button>ORDER NOW</button></Link>
+        
+      </div>
     </div>
   )
 }
